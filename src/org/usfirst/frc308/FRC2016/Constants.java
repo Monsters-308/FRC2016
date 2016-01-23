369dc0d11d0bc6ea7317f01423f67f9514453b00
themaxham <mhamilton@monsters308.org>
2016-01-23 10:26:35 -0500

    Initial Robot Builder Skeleton

---
+++ src/org/usfirst/frc308/FRC2016/Constants.java
@@ -0,0 +1,16 @@
+package org.usfirst.frc308.FRC2016;
+
+public class Constants {
+	
+	//Arm Constants
+	
+	//Chasis Constants
+	
+	//Shooter Constants
+	
+	//Vision Constants
+	
+	//Pneumatic Constants
+	
+
+}
