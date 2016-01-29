f6da8862c827231596038957f80d2b5d38d16d7a
themaxham <mhamilton@monsters308.org>
2016-01-28 20:51:26 -0500

    Added PID Code for Chasis

---
+++ src/org/usfirst/frc308/FRC2016/RobotConstants.java
@@ -0,0 +1,9 @@
+package org.usfirst.frc308.FRC2016;
+
+public class RobotConstants {
+	
+	//Chassis Constants
+	public static double Kp = 0.0;
+	public static double Ki = 0.0;
+	public static double Kd = 0.0;
+}
