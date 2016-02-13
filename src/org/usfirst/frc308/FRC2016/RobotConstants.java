b9560392b8a74313a15e6d84e6e7a840e2390b90
themaxham <mhamilton@monsters308.org>
2016-02-13 14:10:37 -0500

    added PID constants

--- src/org/usfirst/frc308/FRC2016/RobotConstants.java
+++ src/org/usfirst/frc308/FRC2016/RobotConstants.java
@@ -29,11 +29,23 @@ public class RobotConstants {
 	public static double gyroPIDVelocityTolerance = 0.5; //in degrees/second
 
 	// Shooter Constants
-	public static double intakeGrabSpeed = 0.5; // range -1 to 1
-	public static double intakeAdjustSpeed = 0.2;
+	public static double intakeGrabSpeed = 5000; // in rpm
+	public static double intakeAdjustSpeed = 2000; // in rpm
 	public static double intakeShooterSpeed = 0.0;
-	public static double shooterSpeed = 1.0;
+	public static double intakePIDKp = 0.0;
+	public static double intakePIDKi = 0.000001;
+	public static double intakePIDKd = 0.0;
+	public static double intakePIDKf = 1023.0/12300.0; //1023/maximumspeed
+	public static int intakePIDIZone = 0;
+	public static double intakePIDRampRate = 0.0;
+	public static double shooterSpeed = 10000.0;
 	public static double shooterTolerance = 0.0; // units?
+	public static double shooterPIDKp = 0.2;
+	public static double shooterPIDKi = 0.0;
+	public static double shooterPIDKd = 0.0;
+	public static double shooterPIDKf = 1023.0/12300.0; //1023/maximumspeed
+	public static int shooterPIDIZone = 0;
+	public static double shooterPIDRampRate = 0.0;
 
 	// Arm Constants
 	public static double armLowHeight = 0.0;
