c65476579138d07b5896dde9340f8b036317125d
themaxham <mhamilton@monsters308.org>
2016-02-15 21:21:43 -0500

    added intake code

---
+++ src/org/usfirst/frc308/FRC2016/commands/teleopIntake.java
@@ -0,0 +1,7 @@
+package org.usfirst.frc308.FRC2016.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class teleopIntake extends Command{
+
+}
--- src/org/usfirst/frc308/FRC2016/subsystems/Intake.java
+++ src/org/usfirst/frc308/FRC2016/subsystems/Intake.java
@@ -1,12 +1,14 @@
 package org.usfirst.frc308.FRC2016.subsystems;
 
+import org.usfirst.frc308.FRC2016.commands.teleopIntake;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Intake extends Subsystem{
 
 	@Override
 	protected void initDefaultCommand() {
-		
+		setDefaultCommand(new teleopIntake());
 	}
 
 }
